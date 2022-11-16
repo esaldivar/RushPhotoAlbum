66b541001ea24ec042436e6fdfd99ca0dd88b6d7
Eric Saldivar <esaldivar1214@gmail.com>
2022-11-16 11:11:43 -0600

    ci: changed github action to include user

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing Changelog <code>src/App.tsx</code> and save to reload.
+          Testing Changelog again<code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
