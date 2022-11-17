f842d23013daf9d1a32b69dc5a7363b5b4af812a
Eric Saldivar <esaldivar1214@gmail.com>
2022-11-16 21:09:57 -0600

    feat: add album filter and error alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
+import { AlbumI } from "./utils/interfaces";
+import axios from "axios";
+import FilterForm from "./components/FilterForm/FilterForm";
+import styled from "./App.module.css";
 
-function App() {
+
+
+const App = () =>{
+  const [album, setAlbum] = useState<AlbumI[]>([]);
+  const [error, setError] = useState<string>("");
+  const [alert, setAlert] = useState<boolean>(false);
+
+  const fetchAlbum = (albumId:number) => {
+    axios.get(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+    .then((res) => {
+      if(res.data.length === 0){
+        setAlbum([]);
+        setError("No album found. Please try another album id.");
+        setAlert(true);
+      } else {
+        setAlert(false);
+        setAlbum(res.data);
+        setError("");
+      }
+    }).catch((e) => {console.log(e)})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing Changelog<code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styled.App}>
+      <div>
+        <h1>Get Album Details</h1>
+      </div>
+      <FilterForm getAlbum={fetchAlbum} error={error} alert={alert} closeAlert={setAlert}/>
+      {album.map((album) => (
+        <div key={album.id}>
+          <h1>{album.title}</h1>
+          <h1>{album.id}</h1>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/components/FilterForm/FilterForm.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import styled from "./FilterForm.module.css";
+
+interface FilterFormPropsI {
+	getAlbum: (albumId: number) => void;
+	error?: string;
+	alert?: boolean;
+	closeAlert: (set:boolean) => void;
+}
+
+const FilterForm = ({getAlbum , error, alert, closeAlert}: FilterFormPropsI) =>{
+
+	
+	  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+		if (reason === 'clickaway') {
+		  return;
+		}
+		closeAlert(false);
+	  };
+
+  return (
+    <div className={styled.FilterForm}>
+      <TextField
+          id="standard-number"
+          label="Album ID"
+          type="number"
+		  helperText="Enter the album ID"
+		  onChange={(e)=>{getAlbum(parseInt(e.target.value))}}
+          variant="outlined"
+        />
+		{error && 
+		<Snackbar open={alert} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{vertical:'top', horizontal:'center'}} >
+			<Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}  variant="filled">
+				{error}
+			</Alert>
+		  </Snackbar>
+		}
+    </div>
+  );
+}
+
+export default FilterForm;
