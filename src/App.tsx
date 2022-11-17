e5ea99673f335a000422cfa908801f3a4040b1dc
Eric Saldivar <esaldivar1214@gmail.com>
2022-11-16 21:12:00 -0600

    feat: add album filter and alert on invalid album id

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,41 @@ import axios from "axios";
 import FilterForm from "./components/FilterForm/FilterForm";
 import styled from "./App.module.css";
 
-
-
-const App = () =>{
+const App = () => {
   const [album, setAlbum] = useState<AlbumI[]>([]);
   const [error, setError] = useState<string>("");
   const [alert, setAlert] = useState<boolean>(false);
 
-  const fetchAlbum = (albumId:number) => {
-    axios.get(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
-    .then((res) => {
-      if(res.data.length === 0){
-        setAlbum([]);
-        setError("No album found. Please try another album id.");
-        setAlert(true);
-      } else {
-        setAlert(false);
-        setAlbum(res.data);
-        setError("");
-      }
-    }).catch((e) => {console.log(e)})
-  }
+  const fetchAlbum = (albumId: number) => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+      .then((res) => {
+        if (res.data.length === 0) {
+          setAlbum([]);
+          setError("No album found. Please try another album id.");
+          setAlert(true);
+        } else {
+          setAlert(false);
+          setAlbum(res.data);
+          setError("");
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   return (
     <div className={styled.App}>
       <div>
         <h1>Get Album Details</h1>
       </div>
-      <FilterForm getAlbum={fetchAlbum} error={error} alert={alert} closeAlert={setAlert}/>
+      <FilterForm
+        getAlbum={fetchAlbum}
+        error={error}
+        alert={alert}
+        closeAlert={setAlert}
+      />
       {album.map((album) => (
         <div key={album.id}>
           <h1>{album.title}</h1>
@@ -40,6 +47,6 @@ const App = () =>{
       ))}
     </div>
   );
-}
+};
 
 export default App;
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,7 +1,7 @@
 export interface AlbumI {
-	albumId: number;
-    id: number;
-    title: string;
-    url: string;
-    thumbnailUrl: string;
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 }
