0309ebd65c48955733e8df58ccac4bd118643a02
Eric Saldivar <esaldivar1214@gmail.com>
2022-11-17 14:36:13 -0600

    test: add test to App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders title text", () => {
-  render(<App />);
-  const title = screen.getByText(/Get Album Details/i);
-  expect(title).toBeInTheDocument();
+describe("App component", () => {
+  test("renders title text", () => {
+    render(<App />);
+    const title = screen.getByText(/Get Album Details/i);
+    expect(title).toBeInTheDocument();
+  });
+
+  test("renders loading skeleton", async () => {
+    render(<App />);
+    const skeleton = screen.queryAllByRole("listitem");
+    expect(skeleton).toHaveLength(6);
+  });
+
+  test("renders text field on initial load", async () => {
+    render(<App />);
+    const albumLabel = screen.getByLabelText(/Album ID/i);
+    expect(albumLabel).toBeInTheDocument();
+  });
 });
